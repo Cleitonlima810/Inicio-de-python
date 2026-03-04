127a82af5c242ed4ee86cd35a982cda596683e90
Cleiton Barbosa <cleitonlima810@gmail.com>
2026-03-03 21:34:02 -0300

    Coreção e melhoria dos desafio 59

--- python 3 - Modulo 2/desafio0059.py
+++ python 3 - Modulo 2/desafio0059.py
@@ -1,13 +1,10 @@
 from time import sleep
 
-num1 = 0
-num2 = 0
-num3 = -1
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+opção = 0
 
-while num3 != 5:
-    num1 = int(input('Digite o primeiro número: '))
-    num2 = int(input('Digite o segundo número: '))
-
+while opção != 5:
     print('-=-' * 5)
     print('[1]Somar\n'
           '[2]Multiplicar\n'
@@ -16,19 +13,19 @@ while num3 != 5:
           '[5]Sair do Programa')
     print('-=-' * 5)
 
-    num3 = int(input('Qual função deseja executar? '))
+    opção = int(input('Qual função deseja executar? '))
 
-    if num3 == 1:
+    if opção == 1:
         somar = num1 + num2
         print('-=-' * 25)
-        print('Valor da somar entre {} e {} é igual a {}.'.format(num1,num2,somar))
+        print('A soma de {} + {} é {}.'.format(num1,num2,somar))
         print('-=-' * 25)
-    elif num3 == 2:
+    elif opção == 2:
         multiplicar = num1 * num2
         print('-=-' * 25)
-        print('Valor da multiplicação entre {} e {} é igual a {}.'.format(num1,num2,multiplicar))
+        print('O resultado de {} x {} é {}.'.format(num1,num2,multiplicar))
         print('-=-' * 25)
-    elif num3 == 3:
+    elif opção == 3:
         if num1 > num2:
             print('-=-' * 25)
             print('O primeiro número {} é maior que o segundo {}.'.format(num1,num2))
@@ -37,11 +34,15 @@ while num3 != 5:
             print('-=-' * 25)
             print('O segundo número {} é maior que o primeiro {}. '.format(num2, num1))
             print('-=-' * 25)
-    elif num3 == 4:
+    elif opção == 4:
         print('PROGRAMA REINICIANDO...')
         sleep(3)
-    elif num3 == '':
-        print('Ou você digitou letras ou não digitou nada, tente novamente.')
+        num1 = int(input('Digite o primeiro número: '))
+        num2 = int(input('Digite o segundo número: '))
+    elif opção == 5:
+        print('Finalizando...')
         sleep(3)
-if num3 == 5:
-    print('FIM DO PROGRAMA')
+        print('Finalizado com sucesso!')
+    else:
+        print('Opção inválida, tente novamente.')
+        sleep(3)
